058c52ff78ed1173e1c72e57040bc596102702dc
AconLight <w.mosieski@interia.pl>
2018-03-11 21:12:35 +0100

    board setup

--- src/main/java/sudoku/Board.java
+++ src/main/java/sudoku/Board.java
@@ -4,4 +4,18 @@ package sudoku;
  * Created by Wojciech on 2018-03-11.
  */
 public class Board {
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    private int[][] board;
+
+    public Board() {
+        board = new int[9][9];
+    }
+
+    public void fill(int x, int y, int value) {
+
+    }
 }
