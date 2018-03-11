c6728cc52fbd584f2522cb5bc0f4fef229645603
AconLight <w.mosieski@interia.pl>
2018-03-11 22:10:00 +0100

    gupi merge od artura

--- src/main/java/sudoku/Board.java
+++ src/main/java/sudoku/Board.java
@@ -15,6 +15,14 @@ public class Board {
         board = new int[9][9];
     }
 
+    public Board(Board b) {
+        for(int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                this.board[x][y] = b.board[x][y];
+            }
+        }
+    }
+
     //TODO diffrent exceptions
     public void fill(int x, int y, int value) throws Exception {
         if (x < 0 || x >= 9 || y < 0 || y >= 9) throw new Exception();
--- src/main/java/sudoku/Generator.java
+++
@@ -1,8 +0,0 @@
-package sudoku;
-
-/**
- * Created by Wojciech on 2018-03-11.
- */
-public class Generator {
-
-}
