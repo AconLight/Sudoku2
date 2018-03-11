473fb3cf0be6722e1a7a33d9c96725ffdadd6fa1
ArturPrzybysz <prz.artur@gmail.com>
2018-03-11 22:26:22 +0100

    Unit tests in progress

--- src/main/java/sudoku/BoardChecker.java
+++ src/main/java/sudoku/BoardChecker.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Set;
 
 public class BoardChecker {
-
     public boolean checkBoard(Board board) {
         return checkSubMatrices(board) && checkRowsAndColumns(board);
     }
