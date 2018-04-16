a3e903a6239fb10fba8697b4a0c29a249297ea41
dfdsaf <w.mosienski@gmail.com>
2018-04-16 14:10:58 +0200

    fdasfdasfas

--- src/main/java/sudoku/SudokuField.java
+++ src/main/java/sudoku/SudokuField.java
@@ -1,5 +1,8 @@
 package sudoku;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class SudokuField {
     public int getValue() {
         return value;
@@ -14,4 +17,25 @@ public class SudokuField {
     }
 
     private int value;
+
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(13, 37).
+                append(value).
+                toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        SudokuField rhs = (SudokuField) obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(value, rhs.value)
+                .isEquals();
+    }
+
+    public String toString() {
+        return "asd";
+    }
 }
