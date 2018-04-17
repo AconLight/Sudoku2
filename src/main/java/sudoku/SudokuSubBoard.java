00d8deb8e3cc884ace0831e8240685774eeedd8b
pobi <w.mosienski@gmail.com>
2018-04-17 11:25:00 +0200

    dgfasdfdasfsd

--- src/main/java/sudoku/SudokuSubBoard.java
+++ src/main/java/sudoku/SudokuSubBoard.java
@@ -12,17 +12,25 @@ public class SudokuSubBoard {
         //this.fields = new SudokuField[fields.length];
         //this.fields = new List
 
+        SudokuField[] fields2 = new SudokuField[9];
+        this.fields = Arrays.asList(fields2);
+
         for(int i = 0; i < fields.length; i++) {
-            this.fields.add(fields[i]);
+            //this.fields.add(fields[i]);
+            this.fields.set(i, fields[i]);
+
         }
     }
 
     public boolean verify() {
         Set<Integer> usedNumbers = new HashSet<Integer>();
         int count = 0;
+
         for (int i = 0; i < fields.size(); i++) {
+
             if (fields.get(i).getValue() != 0) {
                 usedNumbers.add(fields.get(i).getValue());
+
                 count ++;
             }
         }
@@ -31,19 +39,24 @@ public class SudokuSubBoard {
     }
 
     @Override
-    public int hashCode() {
-        return new HashCodeBuilder(13, 37).
-                append(fields).
-                toHashCode();
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SudokuSubBoard that = (SudokuSubBoard) o;
+        return Objects.equals(fields, that.fields);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        //SudokuSubBoard rhs = (SudokuSubBoard) obj;
-        return false;//fields.containsAll(rhs.fields) && listB.containsAll(fields)
+    public int hashCode() {
+
+        return Objects.hash(fields);
     }
 
+    @Override
     public String toString() {
-        return "asd";
+        return "SudokuSubBoard{" +
+                "fields=" + fields +
+                '}';
     }
+
 }
